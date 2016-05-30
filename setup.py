ea4c27c83719159938d9158946f44f321bfcc601
jgrizou <jonathan.grizou@glasgow.ac.uk>
2016-05-30 11:09:06 +0100

    version in setup automatically handled

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+from pycont._version import __version__
+
 from setuptools import find_packages, setup
 setup(name="pycont",
-      version="0.1",
+      version=__version__,
       description="Tools to work with Tricontinental Pumps",
       author="Jonathan Grizou",
       author_email='jonathan.grizou@glasgow.ac.uk',
