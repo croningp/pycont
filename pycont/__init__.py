839e58eb27917ae4ffdfd5b9d685980e8e07fb84
ShinRa26 <grahamkeenan@hotmail.co.uk>
2016-10-21 14:37:38 +0100

    Added commentary to code for Sphinx

--- pycont/__init__.py
+++ pycont/__init__.py
@@ -1,5 +1,11 @@
+"""
+Custom library designed to control Tricontinent C-series syringe pumps.
+
+.. moduleauthor:: Jonathan Grizou <Jonathan.Grizou@gla.ac.uk>
+
+"""
 from ._version import __version__
-from _logger import __logger_root_name__
+from ._logger import __logger_root_name__
 
 import logging
 logging.getLogger(__logger_root_name__).addHandler(logging.NullHandler())
