36d7b9d8ae53ea3212b8bb99581375291135d662
Dario Cambie <dario.cambie@glasgow.ac.uk>
2020-03-02 17:13:49 +0000

    Adds VirtualMultiPumpController and related class to simulate pumps
    This allows automate testing of pycont-based code.

--- pycont/controller.py
+++ pycont/controller.py
@@ -261,6 +261,27 @@ class PumpIO(object):
             raise err
 
 
+class VirtualPumpIO(PumpIO):
+    def open(self, port, baudrate=DEFAULT_IO_BAUDRATE, timeout=DEFAULT_IO_TIMEOUT):
+        self._serial = None
+
+    def close(self):
+        pass
+
+    def flushInput(self):
+        pass
+
+    def write(self, packet):
+        str_to_send = packet.to_string()
+        self.logger.debug("Virtually sending {}".format(str_to_send))
+
+    def readline(self):
+        raise PumpIOTimeOutError
+
+    def write_and_readline(self, packet):
+        raise PumpIOTimeOutError
+
+
 class PumpIOTimeOutError(Exception):
     """
     Exception for when the response time is greater than the timeout threshold.
@@ -1210,6 +1231,79 @@ class C3000Controller(object):
         self.write_and_read_from_pump(self._protocol.forge_terminate_packet())
 
 
+class VirtualC3000Controller(C3000Controller):
+
+    def write_and_read_from_pump(self, packet, max_repeat=MAX_REPEAT_WRITE_AND_READ):
+        raise NotImplementedError
+
+    def initialize(self, valve_position=None, max_repeat=MAX_REPEAT_OPERATION, secure=True):
+        raise NotImplementedError
+
+    def initialize_valve_right(self, operand_value=0, wait=True):
+        raise NotImplementedError
+
+    def initialize_valve_left(self, operand_value=0, wait=True):
+        raise NotImplementedError
+
+    def initialize_no_valve(self, operand_value=0, wait=True):
+        raise NotImplementedError
+
+    def initialize_valve_only(self, operand_string='0,0', wait=True):
+        raise NotImplementedError
+
+    def is_idle(self):
+        return True
+
+    def is_busy(self):
+        return False
+
+    def is_initialized(self):
+        return True
+
+    def init_all_pump_parameters(self, secure=True):
+        pass
+
+    def set_microstep_mode(self, micro_step_mode):
+        pass
+
+    def set_top_velocity(self, top_velocity, max_repeat=MAX_REPEAT_OPERATION, secure=True):
+        pass
+
+    def get_top_velocity(self):
+        return 10000
+
+    def get_plunger_position(self):
+        return 0
+
+    def pump(self, volume_in_ml, from_valve=None, speed_in=None, wait=False, secure=True):
+        pass
+
+    def deliver(self, volume_in_ml, to_valve=None, speed_out=None, wait=False, secure=True):
+        pass
+
+    def go_to_volume(self, volume_in_ml, speed=None, wait=False, secure=True):
+        return True
+
+    def go_to_max_volume(self, speed=None, wait=False):
+        return True
+
+    def get_valve_position(self, max_repeat=MAX_REPEAT_OPERATION):
+        if self.current_valve_position is not None:
+            return self.current_valve_position
+        else:
+            return VALVE_INPUT
+
+    def set_valve_position(self, valve_position, max_repeat=MAX_REPEAT_OPERATION, secure=True):
+        self.current_valve_position = valve_position
+        return True
+
+    def set_eeprom_config(self, operand_value):
+        pass
+
+    def get_eeprom_config(self):
+        return None
+
+
 class MultiPumpController(object):
     """
     This class deals with controlling multiple pumps on one or more hubs at a time.
@@ -1640,3 +1734,32 @@ class MultiPumpController(object):
             self.parallel_transfer(left_to_pump, from_valve, to_valve, speed_in, speed_out, secure)
         elif wait is True:  # If no more pumping is needed wait if needed
             self.apply_command_to_pumps(list(pumps_and_volumes_dict.keys()), "wait_until_idle")
+
+
+class VirtualMultiPumpController(MultiPumpController):
+    def __init__(self, setup_config):
+        self.logger = create_logger(self.__class__.__name__)
+        self.pumps = {}
+        self._io = []
+
+        # Sets groups and default configs if provided in the config dictionary
+        self.groups = setup_config['groups'] if 'groups' in setup_config else {}
+        self.default_config = setup_config['default'] if 'default' in setup_config else {}
+
+        if "hubs" in setup_config:  # This implements the "new" behaviour with multiple hubs
+            for hub_config in setup_config["hubs"]:
+                # Each hub has its own I/O config. Create a PumpIO object per each hub and reuse it with -1 after append
+                self._io.append(VirtualPumpIO.from_config(hub_config['io']))
+                for pump_name, pump_config in list(hub_config['pumps'].items()):
+                    full_pump_config = self.default_pump_config(pump_config)
+                    self.pumps[pump_name] = VirtualC3000Controller.from_config(self._io[-1], pump_name, full_pump_config)
+        else:  # This implements the "old" behaviour with one hub per object instance / json file
+            self._io = VirtualPumpIO.from_config(setup_config['io'])
+            for pump_name, pump_config in list(setup_config['pumps'].items()):
+                full_pump_config = self.default_pump_config(pump_config)
+                self.pumps[pump_name] = VirtualC3000Controller.from_config(self._io, pump_name, full_pump_config)
+
+        self.set_pumps_as_attributes()
+
+    def smart_initialize(self, secure=True):
+        pass
